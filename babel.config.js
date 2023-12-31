84f4c22ad663238ce2ef1e6d2cb3000358bfe6f6
fuchsily <victoria.fuchs@arcor.de>
2023-12-31 15:51:57 +0100

    added CalendarWeek Componente

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    ["@babel/present-env",
+      {
+        corejs: "core-js@3",
+      },
+    ],
+  ],
+};
---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="calender-week">
+        <div class="card-group">
+        <div class="card" v-for="i in 7" :key="i">
+            <div class="card-header text-center" role="button">
+            <strong>Montag</strong>
+            </div>
+            <div class="card-body">
+            <!-- Anfang: Template für die Calendar-Day-Component -->
+            <div id="calendar-day">
+                <!-- Anfang: Template für die Calendar-Event-Component -->
+                <div id="calendar-event">
+                <div class="alert text-center">
+                    <div>
+                    <strong>Hoch</strong>
+                    </div>
+
+                    <div>Vue.js 3 lernen</div>
+
+                    <div>
+                    <i class="fas fa-edit me-2" role="button"></i>
+                    <i class="far fa-trash-alt" role="button"></i>
+                    </div>
+                </div>
+                </div>
+                <!-- Ende: Template für die Calendar-Event-Component -->
+            </div>
+            <!-- Ende: Template für die Calendar-Day-Component -->
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CalendarWeek",
+}
+</script>
+
+
+// alle styles, die in einer Componente erstellt werden, stehen in der gesamten Anwendung global zur Verfügung. Mit scoped kann man das verhindern
+// scoped funktioniert, indem den Elementen ein data-v- Attribut hinzugefügt wird
+<style scoped>
+
+</style>
